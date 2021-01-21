e5f59b1ebfd5690d7899ea1b95e3385952be86ed
Tom <tom.mueller@teachsurfing.org>
2021-01-21 18:03:29 +0100

    initial commit

--- apps/repro/src/app/app.component.ts
+++ apps/repro/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'repro';
+  noany: any;
+  waytoolongline_waytoolongline_waytoolongline_waytoolongline_waytoolongline_waytoolongline_waytoolongline_waytoolongline_waytoolongline_waytoolongline_waytoolongline_waytoolongline_waytoolongline;
 }
