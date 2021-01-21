183f98ecd0baa0b086b0e227aff2d0b9d9fcdebc
Tom <tom.mueller@teachsurfing.org>
2021-01-21 17:27:00 +0100

    initial commit

---
+++ apps/repro/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rule-repro-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'repro';
+}
